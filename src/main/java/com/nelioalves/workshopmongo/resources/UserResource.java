762a9a7d4a5a6a78895d9f83d7b749225114bb4f
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-07-20 22:51:20 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -32,7 +32,7 @@ public class UserResource {
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<UserDTO> findAll(@PathVariable String id){
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
